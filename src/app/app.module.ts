5c5943f68be9e11c8a0dd35a4b4ac7d1dc565b81
ShaniyaG <shaniyaganesh1018@gmail.com>
2022-08-19 21:45:31 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from '../../app-routing-module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, , AppRoutingModule],
   declarations: [AppComponent, HelloComponent, NavbarComponent],
   bootstrap: [AppComponent],
 })
