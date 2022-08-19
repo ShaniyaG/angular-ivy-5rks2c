bc35d33195da4e46df57f3422db246231161a9f7
ShaniyaG <shaniyaganesh1018@gmail.com>
2022-08-19 22:01:38 +0530

    2 files modified, 7 added

--- app-routing-module.ts
+++ app-routing-module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { CardsComponent } from 'src/app/cards/cards.component';
+import { TableComponent } from 'src/app/table/table.component';
+import { ThemeComponent } from 'src/app/theme/theme.component';
 
-const routes: Routes = [{ path: '', redirectTo: 'home', pathMatch: 'full' }]; // sets up routes constant where you define your routes
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: TableComponent },
+  { path: 'card', component: CardsComponent },
+  { path: 'theme', component: ThemeComponent },
+]; // sets up routes constant where you define your routes
 
 // configures NgModule imports and exports
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,22 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppRoutingModule } from '../../app-routing-module';
+import { CardsComponent } from './cards/cards.component';
+import { TableComponent } from './table/table.component';
+import { ThemeComponent } from './theme/theme.component';
+import { TableService } from './table.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, , AppRoutingModule],
-  declarations: [AppComponent, HelloComponent, NavbarComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavbarComponent,
+    CardsComponent,
+    TableComponent,
+    ThemeComponent,
+  ],
   bootstrap: [AppComponent],
+  providers: [TableService],
 })
 export class AppModule {}
